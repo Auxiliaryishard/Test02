dd1b0fea4e3656781bd335cbcd2e97ca9db3b666
j1074796256 <15846287378@163.com>
2020-08-20 09:42:20 +0800

    提交

---
+++ chongya/src/com/oracle/Main.java
@@ -0,0 +1,13 @@
+package com.oracle;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("chongya");
+        System.out.println("chongya");
+        System.out.println("chongya");
+        System.out.println("chongya");
+
+
+    }
+}
